2944e1f379e26a75180dfd65f91796d70d57f318
Sanket Arun Mule <sanketmu@cybage.com>
2026-04-07 15:49:13 +0530

    provider fixes in progress

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                  = var.azs
-  
+
   providers = {
     aws = aws
   }
